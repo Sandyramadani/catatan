30d70a1e6e3946d0737b96114185a742c9aa0a3e
Sandyramadani <57568232+Sandyramadani@users.noreply.github.com>
2019-11-09 12:50:01 +0800

    flutter

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:catatan/layout/note_list.dart';
+import 'package:flutter/material.dart';
+import 'package:catatan/layout/note_detail.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Catatan Ku',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: NoteList(),
+    );
+  }
+}
